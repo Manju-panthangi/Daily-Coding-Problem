3197e76feb07b062ff66b8e9f13bead5d120ef84
Manjushree Panthangi <manjupanthangi33597@gmail.com>
2022-02-20 01:05:19 +0530

    Create Day1-1.1.java

---
+++ Arrays-1/Day1-1.1.java
@@ -0,0 +1,37 @@
+/*1.1 Get product of all other elements
+
+Given an array of integers, return a new array such that each element at index i of 
+the new array is the product of all the numbers in the original array except the one  at i.  
+For example, if our input was [ 1, 2, 3, 4, 5], the expected output would be [ 120,  60, 40, 30, 24].
+If our input was [3, 2, 1],the expected out put wouldbe [2,  3, 6].  */
+
+
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n= scan.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+
+        int[] newarr = new int[n];
+
+        for(int i=0;i<n;i++){
+            int product = 1;
+            for(int j = 0 ;j<n;j++){
+                if(j!=i){
+                    product = product* arr[j];
+                }
+            }
+            newarr[i]=product;
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(newarr[i]+" ");
+        }
+
+    }
+}
+  
