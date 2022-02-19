2447076cb6f7546919937915b8ef73951b18defb
Manjushree Panthangi <manjupanthangi33597@gmail.com>
2022-02-20 01:11:41 +0530

    With lesser time
    O(n)

--- Arrays-1/Day1-1.1.java
+++ Arrays-1/Day1-1.1.java
@@ -5,7 +5,7 @@ the new array is the product of all the numbers in the original array except the
 For example, if our input was [ 1, 2, 3, 4, 5], the expected output would be [ 120,  60, 40, 30, 24].
 If our input was [3, 2, 1],the expected out put wouldbe [2,  3, 6].  */
 
-
+// O(n^2)
 import java.util.Scanner;
 public class Solution {
     public static void main(String[] args){
@@ -31,7 +31,33 @@ public class Solution {
         for(int i=0;i<n;i++){
             System.out.print(newarr[i]+" ");
         }
-
-    }
 }
+
+// O(n)
+
+import java.util.Scanner;
+public class Solution {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        int n= scan.nextInt();
+        int[] arr = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = scan.nextInt();
+        }
+
+        int[] newarr = new int[n];
+        int prod=1;
+
+        for(int i=0;i<n;i++){
+            prod = arr[i]*prod;
+        }
+        for(int i=0;i<n;i++){
+            newarr[i] = prod/arr[i];
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.print(newarr[i]+" ");
+        }
+}
+
   
